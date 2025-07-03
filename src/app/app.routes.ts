2f5276d4d8903c8c377c54e950a3b2614dc93254
josuebaez61 <josuebaez61@gmail.com>
2025-07-03 18:02:57 -0300

    ADD, routing redirection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,4 +7,9 @@ export const routes: Routes = [
     component: MainLayout,
     loadChildren: () => import('./pages').then((m) => m.routes),
   },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '',
+  },
 ];
